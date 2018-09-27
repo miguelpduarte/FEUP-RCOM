33e2af0da5a4570f531e157aac8ef9b4cae7f235
xRuiAlves <up201606746@fe.up.pt>
2018-09-27 16:58:53 +0100

    Finished receiver

--- aula1/receiver.c
+++ aula1/receiver.c
@@ -7,83 +7,88 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <strings.h>
+#include <string.h>
 #include <unistd.h>
 
 #define BAUDRATE B38400
 #define SERIAL_PORT "/dev/ttyS0"
 #define _POSIX_SOURCE 1 /* POSIX compliant source */
-#define FALSE 0
-#define TRUE 1
-
-volatile int STOP=FALSE;
 
 int main(int argc, char** argv)
 {
-    int fd, res;
-    struct termios oldtio,newtio;
-    char buf[255];
-
+    struct termios old_termio, new_termio;
   /*
     Open serial port device for reading and writing and not as controlling tty
     because we don't want to get killed if linenoise sends CTRL-C.
   */
     
-    fd = open(SERIAL_PORT, O_RDWR | O_NOCTTY );
-    if (fd <0) {
+    int serial_port_fd = open(SERIAL_PORT, O_RDWR | O_NOCTTY );
+    if (serial_port_fd < 0) {
         perror(SERIAL_PORT); 
         exit(-1); 
     }
 
-    if ( tcgetattr(fd,&oldtio) == -1) { /* save current port settings */
+    if ( tcgetattr(serial_port_fd, &old_termio) == -1) { /* save current port settings */
       perror("tcgetattr");
       exit(-1);
     }
 
-    bzero(&newtio, sizeof(newtio));
-    newtio.c_cflag = BAUDRATE | CS8 | CLOCAL | CREAD;
-    newtio.c_iflag = IGNPAR;
-    newtio.c_oflag = 0;
+    bzero(&new_termio, sizeof(new_termio));
+    new_termio.c_cflag = BAUDRATE | CS8 | CLOCAL | CREAD;
+    new_termio.c_iflag = IGNPAR;
+    new_termio.c_oflag = 0;
 
     /* set input mode (non-canonical, no echo,...) */
-    newtio.c_lflag = 0;
+    new_termio.c_lflag = 0;
 
-    newtio.c_cc[VTIME]    = 0;   /* inter-character timer unused */
-    newtio.c_cc[VMIN]     = 5;   /* blocking read until 5 chars received */
-
-
+    new_termio.c_cc[VTIME]    = 0;   /* inter-character timer unused */
+    new_termio.c_cc[VMIN]     = 5;   /* blocking read until 5 chars received */
 
   /* 
     VTIME e VMIN devem ser alterados de forma a proteger com um temporizador a 
-    leitura do(s) pr�ximo(s) caracter(es)
+    leitura do(s) próximo(s) caracter(es)
   */
 
+    tcflush(serial_port_fd, TCIOFLUSH);
 
-
-    tcflush(fd, TCIOFLUSH);
-
-    if ( tcsetattr(fd,TCSANOW,&newtio) == -1) {
+    if ( tcsetattr(serial_port_fd, TCSANOW, &new_termio) == -1) {
       perror("tcsetattr");
       exit(-1);
     }
 
-    printf("New termios structure set\n");
+    char byte, message[4096];
+    size_t num_bytes_read = 0;
+    ssize_t res;
+    while (1) {
+      res = read(serial_port_fd, &byte, 1);
 
+      if (res == -1) {
+        fprintf(stderr, "Error in reading from " SERIAL_PORT "\n");
+        exit(-2);
+      }
 
-    while (STOP==FALSE) {       /* loop for input */
-      res = read(fd,buf,255);   /* returns after 5 chars have been input */
-      buf[res]=0;               /* so we can printf... */
-      printf(":%s:%d\n", buf, res);
-      if (buf[0]=='\0') STOP=TRUE;
+      message[num_bytes_read++] = byte;
+
+      if (byte == '\0') {
+        break;
+      }
     }
 
+    printf("%zd bytes read from the serial port\n", num_bytes_read);
+    printf("Message: %s\n", message);
 
-  /* 
-    O ciclo WHILE deve ser alterado de modo a respeitar o indicado no gui�o 
-  */
-
+    res = write(serial_port_fd, message, strlen(message)+1);
+    if (res == -1) {
+      fprintf(stderr, "Error in writing to " SERIAL_PORT "\n");
+      exit(-3);
+    } else if (res != strlen(message) + 1) {
+      fprintf(stderr, "Error: could not send the whole message!!!\n");
+      exit(-4);
+    }
   
     sleep(2);
-    tcsetattr(fd,TCSANOW,&oldtio);
-    close(fd);
+    tcsetattr(serial_port_fd, TCSANOW, &old_termio);
+    close(serial_port_fd);
+
     return 0;
 }
