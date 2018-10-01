383216482f117f55aaca656e65f5a0b5de7844c4
filipa <filipadurao@gmail.com>
2018-10-01 17:01:59 +0100

    Finished state machine

--- aula2/state.c
+++ aula2/state.c
@@ -17,10 +17,13 @@ void handleEvent(byte msg_byte) {
             handleFlagReceived(msg_byte);
             break;
         case ADDR_RCV:
+            handleAddrReceived(msg_byte);
             break;
         case CTRL_RCV:
+            handleCtrlReceived(msg_byte);
             break;
         case BCC_OK:
+            handleBccOk(msg_byte);
             break;
         case MSG_RECEIVED:
             return;
@@ -45,6 +48,50 @@ static void handleFlagReceived(byte msg_byte) {
             break;
         case MSG_ADDR_EMT: case MSG_ADDR_REC:
             setState(ADDR_RCV);
+            state_machine.addr = msg_byte;
+            break;
+        default:
+            setState(WAITING_FLAG);
+            break;
+    }
+}
+
+static void handleAddrReceived(byte msg_byte) {
+    switch (msg_byte) {
+        case MSG_FLAG:
+            setState(FLAG_RCV);
+            break;
+        case MSG_CTRL_DISC: case MSG_CTRL_SET: 
+        case MSG_CTRL_UA:
+        //TODO remaining controls with parameter
+            setState(CTRL_RCV);
+            state_machine.ctrl = msg_byte;
+            break;
+        default:
+            setState(WAITING_FLAG);
+            break;    
+    }
+}
+
+static void handleCtrlReceived(byte msg_byte) {
+    switch (msg_byte) {
+        case MSG_FLAG:
+            setState(WAITING_FLAG);
+            break;
+        default:
+            if(msg_byte == MSG_BCC1(state_machine.addr, state_machine.ctrl)) {
+                setState(BCC_OK);
+            } else {
+                setState(WAITING_FLAG);
+            }
+            break;
+    }
+}
+
+static void handleBccOk(byte msg_byte) {
+    switch (msg_byte) {
+        case MSG_FLAG:
+            setState(MSG_RECEIVED);
             break;
         default:
             setState(WAITING_FLAG);
