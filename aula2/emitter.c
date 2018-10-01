f463aa27eaf4cb7b2869a26c1bbd2b8965dd50d7
Miguel Duarte <miguelpduarte98@gmail.com>
2018-10-01 09:13:51 +0100

    Started aula2

---
+++ aula2/emitter.c
@@ -0,0 +1,120 @@
+/*Non-Canonical Input Processing*/
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <termios.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <strings.h>
+
+#define BAUDRATE B38400
+#define SERIAL_PORT "/dev/ttyS0"
+#define _POSIX_SOURCE 1 /* POSIX compliant source */
+
+int main(int argc, char **argv) {
+    if (argc != 1) {
+        printf("Usage: %s", argv[0]);
+        exit(1);
+    }
+
+    /*
+    Open serial port device for reading and writing and not as controlling tty
+    because we don't want to get killed if linenoise sends CTRL-C.
+  */
+
+    int serial_port_fd = open(SERIAL_PORT, O_RDWR | O_NOCTTY);
+    if (serial_port_fd < 0) {
+        perror(SERIAL_PORT);
+        exit(-1);
+    }
+
+    struct termios old_termio, new_termio;
+
+    if (tcgetattr(serial_port_fd, &old_termio) == -1) {
+        // save current port settings
+        perror("tcgetattr");
+        exit(-1);
+    }
+
+    bzero(&new_termio, sizeof(new_termio));
+    new_termio.c_cflag = BAUDRATE | CS8 | CLOCAL | CREAD;
+    new_termio.c_iflag = IGNPAR;
+    new_termio.c_oflag = 0;
+
+    // set input mode (non-canonical, no echo,...)
+    new_termio.c_lflag = 0;
+
+    new_termio.c_cc[VTIME] = 0; // inter-character timer unused
+    new_termio.c_cc[VMIN] = 5;  // blocking read until 5 chars received
+
+    /* 
+    VTIME e VMIN devem ser alterados de forma a proteger com um temporizador a 
+    leitura do(s) próximo(s) caracter(es)
+  */
+
+    tcflush(serial_port_fd, TCIOFLUSH);
+
+    if (tcsetattr(serial_port_fd, TCSANOW, &new_termio) == -1) {
+        perror("tcsetattr");
+        exit(-1);
+    }
+
+    char *buf = NULL;
+    size_t n;
+    ssize_t n_bytes;
+
+    printf("Enter a message: ");
+
+    n_bytes = getline(&buf, &n, stdin);
+    if (n_bytes == -1) {
+        fprintf(stderr, "Error in reading from stdin");
+        exit(-2);
+    }
+
+    buf[n_bytes-1] = '\0';
+
+    n_bytes = write(serial_port_fd, buf, n_bytes * sizeof(buf[0]));
+    if (n_bytes == -1) {
+        fprintf(stderr, "Failed to write in serial port");
+        exit(-3);
+    }
+
+    printf("%zd bytes written to serial port\n", n_bytes);
+    //Waiting to ensure that the data is sent before resetting SP configurations
+    
+    char message[4096];
+    char byte;
+    n = 0;
+
+    while(1){
+        n_bytes = read(serial_port_fd, &byte, 1);
+
+        if(n_bytes == -1){
+            fprintf(stderr, "Error reading return message");
+            exit(-4);
+        }
+
+        message[n++] = byte;
+
+        if(byte == '\0'){
+            break;
+        }
+    }
+
+    printf("\nWaiting message...\n");
+    printf("Message returned: %s\n", message);
+    
+    sleep(2);
+
+    free(buf);
+
+    if (tcsetattr(serial_port_fd, TCSANOW, &old_termio) == -1) {
+        perror("tcsetattr reset");
+        exit(-1);
+    }
+
+    close(serial_port_fd);
+    return 0;
+}
---
+++ aula2/receiver.c
@@ -0,0 +1,100 @@
+/*Non-Canonical Input Processing*/
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <termios.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <strings.h>
+#include <string.h>
+#include <unistd.h>
+
+#define BAUDRATE B38400
+#define SERIAL_PORT "/dev/ttyS0"
+#define _POSIX_SOURCE 1 /* POSIX compliant source */
+
+int main(int argc, char** argv)
+{
+    struct termios old_termio, new_termio;
+  /*
+    Open serial port device for reading and writing and not as controlling tty
+    because we don't want to get killed if linenoise sends CTRL-C.
+  */
+    
+    int serial_port_fd = open(SERIAL_PORT, O_RDWR | O_NOCTTY );
+    if (serial_port_fd < 0) {
+        perror(SERIAL_PORT); 
+        exit(-1); 
+    }
+
+    if ( tcgetattr(serial_port_fd, &old_termio) == -1) { /* save current port settings */
+      perror("tcgetattr");
+      exit(-1);
+    }
+
+    bzero(&new_termio, sizeof(new_termio));
+    new_termio.c_cflag = BAUDRATE | CS8 | CLOCAL | CREAD;
+    new_termio.c_iflag = IGNPAR;
+    new_termio.c_oflag = 0;
+
+    /* set input mode (non-canonical, no echo,...) */
+    new_termio.c_lflag = 0;
+
+    new_termio.c_cc[VTIME]    = 0;   /* inter-character timer unused */
+    new_termio.c_cc[VMIN]     = 5;   /* blocking read until 5 chars received */
+
+  /* 
+    VTIME e VMIN devem ser alterados de forma a proteger com um temporizador a 
+    leitura do(s) próximo(s) caracter(es)
+  */
+
+    tcflush(serial_port_fd, TCIOFLUSH);
+
+    if ( tcsetattr(serial_port_fd, TCSANOW, &new_termio) == -1) {
+      perror("tcsetattr");
+      exit(-1);
+    }
+
+    printf("Waiting message ...\n");
+
+    char byte, message[4096];
+    size_t num_bytes_read = 0;
+    ssize_t res;
+    while (1) {
+      res = read(serial_port_fd, &byte, 1);
+
+      if (res == -1) {
+        fprintf(stderr, "Error in reading from " SERIAL_PORT "\n");
+        exit(-2);
+      }
+
+      message[num_bytes_read++] = byte;
+
+      if (byte == '\0') {
+        break;
+      }
+    }
+
+    printf("\n%zd bytes read from the serial port\n", num_bytes_read);
+    printf("Message: %s\n", message);
+
+    printf("\nSending message ...\n");
+
+    res = write(serial_port_fd, message, strlen(message)+1);
+    if (res == -1) {
+      fprintf(stderr, "Error in writing to " SERIAL_PORT "\n");
+      exit(-3);
+    } else if (res != strlen(message) + 1) {
+      fprintf(stderr, "Error: could not send the whole message!!!\n");
+      exit(-4);
+    }
+
+    printf("Message sent.\n");
+  
+    sleep(2);
+    tcsetattr(serial_port_fd, TCSANOW, &old_termio);
+    close(serial_port_fd);
+
+    return 0;
+}
