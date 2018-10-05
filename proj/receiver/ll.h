4bd6b8ff09c1a9cb76ab70855e29c0482a84024d
xRuiAlves <up201606746@fe.up.pt>
2018-10-05 18:03:41 +0100

    Organized project folder

---
+++ proj/receiver/ll.h
@@ -0,0 +1,48 @@
+#ifndef _LL_
+#define _LL_
+
+#include "message_defines.h"
+
+#define TRANSMITTER 0
+#define RECEIVER    1
+
+// Errors
+#define WRITE_SUPERVISION_MSG_FAILED        -1
+#define READ_SUPERVISION_MSG_FAILED         -2
+#define INVALID_RECEIVER_ACKNOWLEDGEMENT    -3
+#define INVALID_COMMUNICATION_ROLE          -4
+
+/**
+ * @brief   opens data connection connection
+ * @param   fd      data connection file descriptor
+ * @param   role    communication role, TRASMITTER(0) | RECEIVER(1)
+ * @return  data connection identifier on success, negative number on failure
+*/
+int llopen(int fd, byte role);
+
+/**
+ * @brief   closes data connection
+ * @param   fd  data connection file descriptor
+ * @return  positive number on success, negative number on failure
+*/
+int llclose(int fd);
+
+/**
+ * @brief   writes a message to the data connection
+ * @param   fd      data connection file descriptor
+ * @param   buffer  buffer message
+ * @param   length  buffer message number of bytes
+ * @return  number of written bytes, negative number on failure
+*/
+int llwrite(int fd, byte* buffer, int length);
+
+
+/**
+ * @brief   reads a message from the data connection
+ * @param   fd      data connection file descriptor
+ * @param   buffer  read buffer message
+ * @return  number of read bytes, negative number on failure
+*/
+int llread(int fd, byte* buffer);
+
+#endif /* _LL_ */
---
+++ proj/receiver/message_defines.h
@@ -0,0 +1,39 @@
+#ifndef _MESSAGE_DEFINES_
+#define _MESSAGE_DEFINES_
+
+#define BIT(n) (0x01<<(n))
+
+//flag
+#define MSG_FLAG                0x7E    /* Flag indicating message start and message end */
+#define MSG_FLAG_START_IDX      0
+#define MSG_FLAG_END_IDX        4
+
+//address
+#define MSG_ADDR_EMT    0x03    /* Command sent by the emitter and responses sent by the receiver */
+#define MSG_ADDR_REC    0x01    /* Command sent by the receiver and responses sent by the emitter */
+#define MSG_ADDR_IDX    1
+
+//control
+#define MSG_CTRL_SET    0x03                        /* Setup */
+#define MSG_CTRL_DISC   0x0B                        /* Disconnect */
+#define MSG_CTRL_UA     0x07                        /* Unnumbered Acknowledgement */
+#define MSG_CTRL_RR(r)  0x05 | (r ? BIT(7) : 0x00)  /* Receiver Ready (positive ACK) */
+#define MSG_CTRL_REJ(r) 0x01 | (r ? BIT(7) : 0x00)  /* Reject (negative ACK) */
+#define MSG_CTRL_IDX    2
+
+//bcc
+#define MSG_BCC1(addr, ctrl)    ((addr) ^ (ctrl))   /* Message Parity */
+#define MSG_BCC1_IDX    3
+
+//message configurations
+#define MSG_WAIT_TIME               30       /* Will wait 3 seconds */
+#define MSG_MIN_CHARS               0        /* If MIN = 0 and TIME > 0, TIME serves as a timeout value. The read will be satisfied 
+                                                    if a single character is read, or TIME is exceeded (t = TIME *0.1 s). If TIME is 
+                                                    exceeded, no character will be returned */
+#define MSG_NUM_READ_TRIES          3        /* Number of times reading the response is tried. After these number of tries, abort */
+#define MSG_SUPERVISION_MSG_SIZE    5        /* Size of the supervision message */   
+
+//other
+typedef unsigned char byte;
+
+#endif /* _MESSAGE_DEFINES_ */
