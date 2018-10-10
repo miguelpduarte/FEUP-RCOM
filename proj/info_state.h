94ea9bfb963565c3d8ae268c3e07bc8fb1342529
xRuiAlves <up201606746@fe.up.pt>
2018-10-10 18:59:30 +0100

    Started info_state

--- proj/info_state.h
+++ proj/info_state.h
@@ -3,4 +3,16 @@
 
 #include "message_defines.h"
 
+typedef enum {WAITING_FLAG=0, FLAG_RCV, ADDR_RCV, CTRL_RCV, BCC_OK, RECEIVING_DATA, MSG_RECEIVED} state_st;
+
+typedef struct {
+    state_st current_state;
+    byte addr;
+    byte ctrl;
+} state_machine_st;
+
+state_st getState();
+
+void handleMsgByte(byte msg_byte);
+
 #endif /* _INFO_STATE_ */
