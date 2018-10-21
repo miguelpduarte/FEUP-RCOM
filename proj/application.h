1cb6cebe7877e80828414eef8a6f7c7394b2cb10
xRuiAlves <up201606746@fe.up.pt>
2018-10-21 18:08:05 +0100

    Started application layer defines

---
+++ proj/application.h
@@ -0,0 +1,24 @@
+#ifndef _APPLICATION_H_
+#define _APPLICATION_H_
+
+#include <stdlib.h>
+
+#define BIT(n) (0x01<<(n))
+
+// control 
+#define APP_CTRL_IDX        0
+#define APP_CTRL_DATA       1
+#define APP_CTRL_START      2
+#define APP_CTRL_END        3
+
+
+
+
+#define APP_SEQ_NUM_IDX     1
+#define APP_LENGTH_MSB_IDX  2
+#define APP_LENGTH_LSB_IDX  3
+#define APP_DATA_START_IDX  4
+
+
+
+#endif /* _APPLICATION_H_ */
